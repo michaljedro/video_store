e6b0423410d89724342c7cf11cceb0426cd5c6de
Michał Jędro <michal.jedro@wp.pl>
2024-07-15 15:28:19 +0200

    update backend

--- backend/main.js
+++ backend/main.js
@@ -23,23 +23,18 @@ app.get("/", (request, response) => {
     return response.status(200).send("Welcome to mern stack tutorial");
 });
 
-app.post('/movie', async (request, response) => { // Zmieniona ścieżka na /movie
+app.post('/movie', async (request, response) => {
     try {
         console.log('POST /movie request received');
         const { title, author, Year } = request.body;
-        
+
         if (!title || !author || !Year) {
             return response.status(400).send({
                 message: 'Send all required fields: title, author, Year',
             });
         }
-        
-        const newVideo = {
-            title,
-            author,
-            Year,
-        };
 
+        const newVideo = { title, author, Year };
         const video = await Video.create(newVideo);
         return response.status(201).send(video);
     } catch (error) {
@@ -47,3 +42,33 @@ app.post('/movie', async (request, response) => { // Zmieniona ścieżka na /mov
         response.status(500).send({ message: error.message });
     }
 });
+
+app.get('/movie/:id', async (request, response) => {
+    try {
+        const movieID = request.params.id;
+
+        // Sprawdź, czy ID jest prawidłowym ObjectId
+        if (!mongoose.Types.ObjectId.isValid(movieID)) {
+            return response.status(400).send({ message: 'Invalid ID format' });
+        }
+
+        const movie = await Video.findById(movieID);
+
+        if (!movie) {
+            return response.status(404).send({ message: 'Movie not found' });
+        }
+
+        return response.status(200).json(movie);
+    } catch (error) {
+        console.error('Error handling GET /movie:', error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+app.put('moive/:id', async (request,response)=> {
+    try {
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
