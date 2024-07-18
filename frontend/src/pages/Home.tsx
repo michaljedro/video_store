0231114118cc52da082a3b41f22fae7bb74fca89
Michał Jędro <michal.jedro@wp.pl>
2024-07-18 18:16:48 +0200

    add axios

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 interface HomeProps {
   navigateTo: (page: 'create' | 'delete' | 'edit' | 'home' | 'show') => void;
 }
 
 export const Home: React.FC<HomeProps> = ({ navigateTo }) => {
+  const [movie,setMovies] = useState<string[]>([])
+  const [loading,setLoading] = useState(false)
+  useEffect(()=> {
+    setLoading(true)
+  })
   return (
     <div>
       <h1>Home Page</h1>
